3f029c35c1f7b18d5d552d29db28d2bca72a9945
Jordan Addis <jordanaddis@gmail.com>
2022-02-07 15:07:59 +0000

    Created a function to open the db connection, and a function to query the db and retrieve data to an assoc array

--- functions.php
+++ functions.php
@@ -1,1 +1,12 @@
 <?php
+
+function getDB(): object {
+    return new PDO('mysql:host=db; dbname=jordan-collection', 'root', 'password');
+}
+
+function getData(object $db): array {
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $query = $db->prepare('SELECT `albumName`, `artistName`, `genre`, `purchaseDate`, `albumImage` FROM `records`;');
+    $query->execute();
+    return $query->fetchAll();
+}
