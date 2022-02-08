e7e9b477a5c49b0b92e1c11d2825decf33933d2b
Jordan Addis <jordanaddis@gmail.com>
2022-02-08 14:08:53 +0000

    Added basic HTML

--- index.php
+++ index.php
@@ -4,5 +4,20 @@ require_once 'functions.php';
 
 $db = getDB();
 $data = getData($db);
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>My Record Collection</title>
+    <link href="./style.css" type="css/text" rel="stylesheet">
+</head>
+<body>
 
-echo outputRecords($data);
+<div style="collectionContainer">
+<?php echo outputRecords($data);?>
+</div>
+
+</body>
+</html>
+
