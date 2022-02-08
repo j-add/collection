f488c727fe60bfd2c362d096738073c0e0b2deb5
Jordan Addis <jordanaddis@gmail.com>
2022-02-08 15:26:58 +0000

    Fixed link to stylesheet. Added grid and basic styling.

--- index.php
+++ index.php
@@ -5,16 +5,18 @@ require_once 'functions.php';
 $db = getDB();
 $data = getData($db);
 ?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>My Record Collection</title>
-    <link href="./style.css" type="css/text" rel="stylesheet">
+    <link href="style.css" type="text/css" rel="stylesheet" />
+    <link href="normalize.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
 
-<div style="collectionContainer">
+<div class="collectionContainer">
 <?php echo outputRecords($data);?>
 </div>
 
