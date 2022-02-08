f5515c23bb46085789a86082bf07ea46c26da6a1
Jordan Addis <jordanaddis@gmail.com>
2022-02-08 16:48:33 +0000

    Changed grid to flexbox and added colors to CSS. Added favicon

--- index.php
+++ index.php
@@ -13,8 +13,15 @@ $data = getData($db);
     <title>My Record Collection</title>
     <link href="style.css" type="text/css" rel="stylesheet" />
     <link href="normalize.css" type="text/css" rel="stylesheet" />
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 </head>
 <body>
+<nav>
+<h1>My Record Collection</h1>
+</nav>
 
 <div class="collectionContainer">
 <?php echo outputRecords($data);?>
