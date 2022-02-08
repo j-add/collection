30c7f23d9590ea5119cb27fa66bde65b4f9ee622
Jordan Addis <jordanaddis@gmail.com>
2022-02-08 21:05:16 +0000

    More stylish

--- index.php
+++ index.php
@@ -11,16 +11,19 @@ $data = getData($db);
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>My Record Collection</title>
-    <link href="style.css" type="text/css" rel="stylesheet" />
     <link href="normalize.css" type="text/css" rel="stylesheet" />
+    <link href="style.css" type="text/css" rel="stylesheet" />
     <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
 </head>
 <body>
 <nav>
-<h1>My Record Collection</h1>
+<h1>🎶 My Record Collection 🎶</h1>
 </nav>
 
 <div class="collectionContainer">
